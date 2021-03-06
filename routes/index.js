c661401572743bbfa4eed7b7bc9b95271ff2410f
hir1m <vanilia.miring@gmail.com>
2021-03-06 09:46:51 -0800

    gcloud deploy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const { render } = require('jade');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('https://github.com/parkanywhereonline');
+  res.send("google cloud deploy");
+  // res.redirect('https://github.com/parkanywhereonline');
 });
 
 module.exports = router;
